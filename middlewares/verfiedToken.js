0f2cfaf3400f04a210014d5843410ad6864fe8fa
joker-0-7 <93847410+joker-0-7@users.noreply.github.com>
2023-12-25 20:21:37 +0200

    Update verfiedToken.js

--- middlewares/verfiedToken.js
+++ middlewares/verfiedToken.js
@@ -4,6 +4,7 @@ const verifyToken = (req, res, next) => {
     req.headers["Authorization"] || req.headers["authorization"];
   try {
     const decoded = jwt.verify(authorization, process.env.SECRET_TOKEN);
+    req.current = decoded.id
     next();
   } catch (err) {
     return res.status(401).json({ msg: "يباشا سجل من اول وجديد" });
