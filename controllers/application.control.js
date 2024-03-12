3f9353c779a9f66062cb890b269e3867f7412607
joker-0-7 <93847410+joker-0-7@users.noreply.github.com>
2024-03-12 03:04:15 +0200

    Update application.control.js

--- controllers/application.control.js
+++ controllers/application.control.js
@@ -23,7 +23,6 @@ const Login = async (req, res) => {
   const token = jwt.sign({ num, id }, process.env.SECRET_TOKEN);
   user.password = undefined;
   user.squad = user.squad.split(" ")[1];
-  if
 user.seatingNumbers = user.seatingNumbers.split("").map(Number);
 user.committeeNumber = user.committeeNumber.split("").map(Number);
   console.log(user.seatingNumbers);
