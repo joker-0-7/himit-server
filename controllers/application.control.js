fcdf1b00070c927b684c1dff3bc11394ded1451c
joker-0-7 <93847410+joker-0-7@users.noreply.github.com>
2024-03-15 00:51:31 +0200

    Update application.control.js

--- controllers/application.control.js
+++ controllers/application.control.js
@@ -122,7 +122,7 @@ const addCumulative = async (req, res) => {
   const data = await cumulativeModel.findById("65ccc209dad0ad14d494da15");
   const userData = data.user;
   const currentData = userData[currentUser];
-  currentData.military = currentData.military.split("-")[0];
+  currentData.military = currentData.military;
   console.log(currentData);
   return res.status(201).json(currentData);
 };
